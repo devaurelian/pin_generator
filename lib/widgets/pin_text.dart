4beeb612cd2931b1daf617a4e66b30c6b24a3796
anaurelian <develop@anaurelian.com>
2020-03-08 19:24:51 +0200

    Try PIN text font sizes

--- lib/widgets/pin_text.dart
+++ lib/widgets/pin_text.dart
@@ -56,14 +56,32 @@ class _PINTextState extends State<PINText> {
   /// Describes the [PINText] widget user interface.
   @override
   Widget build(BuildContext context) {
+
+//    List<double> fontSizes = new List(9);
+//    fontSizes.fillRange(0, 4, 112.0);
+//    fontSizes.fillRange(4, 5, 96.0);
+//    fontSizes.fillRange(5, 9, 60.0);
+//    List<double> fontSizes = [192.0, 144.0, 112.0, 112.0, 96.0, 60.0, 60.0, 60.0, 60.0];
+//    List<double> fontSizes = [192.0, 144.0, 128.0, 120.0, 96.0, 60.0, 60.0, 60.0, 60.0];
+    List<double> fontSizes = [192.0, 144.0, 128.0, 120.0, 96.0, 72.0, 64.0, 60.0, 48.0];
+    print(fontSizes);
+    print(fontSizes[_pin.toString().length - 1]);
+
     return AnimatedOpacity(
       duration: Duration(milliseconds: 500),
       opacity: _opacity,
       child: Text(
         _pin.toString(),
-        style: _pin.toString().length <= 5
-            ? Theme.of(context).textTheme.display4
-            : Theme.of(context).textTheme.display3,
+//        style: _pin.toString().length <= 5
+//            ? Theme.of(context).textTheme.display4
+//            : Theme.of(context).textTheme.display3,
+        style: Theme.of(context).textTheme.display4.copyWith(
+//          fontSize: MediaQuery.of(context).size.width / _pin.toString().length,
+//          fontSize: MediaQuery.of(context).size.shortestSide / _pin.toString().length.clamp(2, 6),
+          fontSize: fontSizes[_pin.toString().length - 1],
+//          fontSize: _pin.toString().length <= 4 ? 112.0 : 60.0,
+        )
+
       ),
     );
   }
